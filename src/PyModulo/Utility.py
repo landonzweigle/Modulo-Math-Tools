262018bc3e04042a772842585612eee0d7f5fe24
Landon Zweigle <lzweigle@rams.colostate.edu>
2021-10-14 16:46:56 -0600

    Created Several scripts and methods.

---
+++ src/PyModulo/Utility.py
@@ -0,0 +1,12 @@
+﻿#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+def get_primes(n=100):
+    _ret = []
+    for i in range(2,n+1):
+        if(is_prime(i)):
+            _ret.append(i)
+    return _ret
+
+def is_prime(n):
+    return len([False for j in range(2, n) if n%j==0])==0
---
+++ src/PyModulo/__init__.py
@@ -0,0 +1,8 @@
+﻿#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyModulo.PyModulo import *
+from PyModulo.Utility import *
+
+from PyModulo.ShanksAlgo import shanks_algorithm
+
