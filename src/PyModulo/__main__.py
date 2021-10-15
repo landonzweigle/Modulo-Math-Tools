7e83b681a76a437d2fcdd234fa2fa1bf53fc9f1f
Landon Zweigle <lzweigle@rams.colostate.edu>
2021-10-15 14:09:02 -0600

    Added desktop.ini to .gitignore and removed it from cache

--- src/PyModulo/__main__.py
+++ src/PyModulo/__main__.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
 	print(flipped_congruence(-5, 32), flipped_congruence(flipped_congruence(-5, 32), 32))
 	print(shanks_algorithm(156, 116, 593))
 	print(get_gcd_fast(423,191))
+
